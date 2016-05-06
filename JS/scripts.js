be05e538daa0916bc02085a9b043a90a1e34c434
mmeravy <mmeravy@localhost.localdomain>
2016-05-06 19:06:50 +0200

    insert new items

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,19 +1,23 @@
 $( document ).ready(function() {
-    var page = window.location.pathname;
+    $("#service-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".content-services").offset().top
+        }, 1000);
+    });
     
-    if(document.URL.indexOf("sluzby.html") >= 0){
-        $('body').css('background-image', 'none');
-        $('.menu-background').css('background-color','rgba(0, 0, 0, 1)');
-        $('.content-background').css('background', '#360000');
-        $('.content-background').css('background', '-moz-linear-gradient(top,  #360000 0%, #fd0000 100%)'); 
-        $('.content-background').css('background', '-webkit-linear-gradient(top,  #360000 0%,#fd0000 100%)');
-        $('.content-background').css('background', 'linear-gradient(to bottom,  #360000 0%,#fd0000 100%)');
-        $('.content-background').css('filter', 'progid:DXImageTransform.Microsoft.gradient( startColorstr="#360000", endColorstr="#fd0000",GradientType=0 )'); 
-    }
-    if(document.URL.indexOf("auta.html") >= 0){
-        $('body').css({'background-image' : "url('Images/desert.jpg')"});
-    }
+    $("#cars-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".content-cars").offset().top
+        }, 1000);
+    });
+    
+    $("#home-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".content-home").offset().top
+        }, 1000);
+    });
+    
+    
 });
-
 
 
