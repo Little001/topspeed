92ab12b8751032ba9a7fbeca2d550696e18f0175
merynek <meravy.martin@gmail.com>
2017-12-07 15:25:41 +0100

    7.12

--- JS/reservation.js
+++ JS/reservation.js
@@ -4,69 +4,107 @@ $( document ).ready(function() {
     var isActive = false,
         dayData;
 
-    $("#datepicker").datepicker().on('changeMonth', function(){ 
-        var listElement = $(".content-reservation .right-content .time-table");
+    //get data
+    getDayData();
+
+    $("#reservation-time ul li").click(function() {
+        $("#reservation-time ul li").removeClass("selected");
+        if (!$(this).hasClass("reservation")) {
+            $(this).addClass("selected");
+        }
+    })
+
+    $("#datepicker").datepicker().on('changeMonth', function() { 
+        var listElement = $("#reservation-time ul");
 
         listElement.find("li").removeClass("reservation");
         getDayData();
     });
 
     $("#datepicker").datepicker().on('changeDate', function() {
-        var listElement = $(".content-reservation .right-content .time-table"),
-            i;
+        var listElement = $("#reservation-time ul"),
+            id,
+            i
 
         listElement.find("li").removeClass("reservation");
         for(i = 0; i < dayData.length; i++) {
+            
             switch(dayData[i]) {
-                case "8:00 - 8:30": listElement.find("#1").addClass("reservation")
+                case "8:00 - 8:30":
+                    id="1";
                     break;
-                case "8:30 - 9:00": listElement.find("#2").addClass("reservation")
+                case "8:30 - 9:00": 
+                    id="2";
                     break;
-                case "9:00 - 9:30": listElement.find("#3").addClass("reservation")
+                case "9:00 - 9:30": 
+                    id="3";
                     break;
-                case "9:30 - 10:00": listElement.find("#4").addClass("reservation")
+                case "9:30 - 10:00": 
+                    id="4";
                     break;
-                case "10:00 - 10:30": listElement.find("#5").addClass("reservation")
+                case "10:00 - 10:30": 
+                    id="5";
                     break;
-                case "10:30 - 11:00": listElement.find("#6").addClass("reservation")
+                case "10:30 - 11:00": 
+                    id="6";
                     break;
-                case "11:00 - 11:30": listElement.find("#7").addClass("reservation")
+                case "11:00 - 11:30":
+                    id="7";
                     break;
-                case "11:30 - 12:00": listElement.find("#8").addClass("reservation")
+                case "11:30 - 12:00": 
+                    id="8";
                     break;
-                case "12:00 - 12:30": listElement.find("#9").addClass("reservation")
+                case "12:00 - 12:30": 
+                    id="9";
                     break;
-                case "12:30 - 13:00": listElement.find("#10").addClass("reservation")
+                case "12:30 - 13:00": 
+                    id="10";
                     break;
-                case "13:00 - 13:30": listElement.find("#11").addClass("reservation")
+                case "13:00 - 13:30": 
+                    id="11";
                     break;
-                case "13:30 - 14:00": listElement.find("#12").addClass("reservation")
+                case "13:30 - 14:00": 
+                    id="12";
                     break;
-                case "14:00 - 14:30": listElement.find("#13").addClass("reservation")
+                case "14:00 - 14:30": 
+                    id="13";
                     break;
-                case "14:30 - 15:00": listElement.find("#14").addClass("reservation")
+                case "14:30 - 15:00": 
+                    id="14";
                     break;
-                case "15:00 - 15:30": listElement.find("#15").addClass("reservation")
+                case "15:00 - 15:30": 
+                    id="15";
                     break;
-                case "15:30 - 16:00": listElement.find("#16").addClass("reservation")
+                case "15:30 - 16:00": 
+                    id="16";
                     break;
-                case "16:00 - 16:30": listElement.find("#17").addClass("reservation")
+                case "16:00 - 16:30": 
+                    id="17";
                     break;
-                case "16:30 - 17:00": listElement.find("#18").addClass("reservation")
+                case "16:30 - 17:00": 
+                    id="18";
                     break;
-                case "17:00 - 17:30": listElement.find("#19").addClass("reservation")
+                case "17:00 - 17:30": 
+                    id="1";
                     break;
-                case "17:30 - 18:00": listElement.find("#20").addClass("reservation")
+                case "17:30 - 18:00": 
+                id="1";
                     break;
-                case "18:00 - 18:30": listElement.find("#21").addClass("reservation")
+                case "18:00 - 18:30": 
+                id="1";
                     break;
-                case "18:30 - 19:00": listElement.find("#22").addClass("reservation")
+                case "18:30 - 19:00": 
+                id="1";
                     break;
-                case "19:00 - 19:30": listElement.find("#23").addClass("reservation")
+                case "19:00 - 19:30": 
+                id="1";
                     break;
-                case "19:30 - 20:00": listElement.find("#24").addClass("reservation")
+                case "19:30 - 20:00": 
+                id="1";
                     break;
             }
+            listElement.find("#"+id).addClass("reservation");
+            listElement.find("#"+id).removeClass("selected");
         };
     });
 
